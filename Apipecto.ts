5d8fc6b1e9baf8c723627e33a87b287a7ab3f8f5
Horhof <hor@hof.hh>
2018-09-15 17:29:33 -0400

    wip

--- Apipecto.ts
+++ Apipecto.ts
@@ -1,5 +1,7 @@
 /** What part of the data is described? */
-enum DescriptionType {
+enum DescType {
+  /* The data is already implied in the selector. */
+  IMPLICIT,
   /* A boolean */
   SWITCH,
   /* Single thing, primitive or container */
@@ -10,12 +12,6 @@ enum DescriptionType {
   ASSOCIATION,
 }
 
-interface Term {
-  optional: boolean;
-  callable: boolean;
-  async: boolean;
-}
-
 interface Behavior {
   selector?: string;
   inputs: Term[];
@@ -23,15 +19,65 @@ interface Behavior {
   pureFunction: boolean;
 }
 
-interface Data extends Term {
-  description: string;
-  descriptionType: DescriptionType;
+interface BasicTerm {
+  optional: boolean;
+  callable: boolean;
+  async: boolean;
+}
+
+interface Data extends BasicTerm {
+  desc?: string;
+  descType: DescType;
   callable: false;
 }
 
-interface Lambda extends Term, Behavior {
+interface Lambda extends BasicTerm, Behavior {
   callable: true;
 }
+
+type Term = Data | Lambda;
+
+type Interface = Behavior
+
+interface Block extends Lambda {
+  selector: string;
+  input: [];
+  output: undefined;
+  pureFunction: false;
+}
+
+interface Callback extends Lambda {
+  selector: undefined;
+  input: Term[];
+  output: undefined;
+  pureFunction: false;
+}
+
+interface Transform extends Lambda {
+  selector: undefined;
+  input: Term[];
+  output: Term;
+  pureFunction: true;
+}
+
+// - Create HTTP request: URI
+const interface00: Behavior = {
+  selector: 'Create HTTP request',
+  inputs: [{
+    desc: 'URI',
+    descType: DescType.SINGLE,
+    optional: false,
+    callable: false,
+    async: false,
+  }],
+  output: {
+    descType: DescType.IMPLICIT,
+    optional: false,
+    callable: false,
+    async: false,
+  },
+  pureFunction: false,
+};
 
 const block: Lambda = {
   optional: false,
