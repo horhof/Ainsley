66a327686ab301948e51c09b66252555b7df839c
Horhof <hor@hof.hh>
2018-11-24 22:49:59 -0500

    Update siteConfig and sidebar

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,72 +1,32 @@
 const siteConfig = {
   title: 'Apipecto',
-  tagline: 'Easy API notation',
-  url: 'https://horhof.github.io/Apipecto/docs',
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
-
-  // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
-
-  // For no header links in the top nav bar -> headerLinks: [],
+  url: 'https://horhof.github.io/Apipecto/',
+  baseUrl: '/',
+  projectName: 'apipecto',
+  organizationName: 'horhof-research',
   headerLinks: [
-    {doc: 'introduction', label: 'Docs'},
-    {page: 'help', label: 'Help'},
+    { doc: 'introduction', label: 'Docs' }
   ],
-
-  /* path to images for header/footer */
   headerIcon: 'img/docusaurus.svg',
   footerIcon: 'img/docusaurus.svg',
   favicon: 'img/favicon.png',
-
-  /* Colors for website */
   colors: {
     primaryColor: '#2E8555',
     secondaryColor: '#205C3B',
   },
-
-  /* Custom fonts for website */
   fonts: {
     heading: ['Allegreya'],
     paragraph: ['Open Sans'],
     code: ['Fira Code Retina']
   },
-
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
-
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: 'monokai',
   },
-
-  // Add custom scripts here that would be placed in <script> tags.
   scripts: ['https://buttons.github.io/buttons.js'],
-
-  // On page navigation for the current documentation page.
   onPageNav: 'separate',
-  // No .html extensions for paths.
   cleanUrl: true,
-
-  // Open Graph and Twitter card images.
   ogImage: 'img/docusaurus.png',
   twitterImage: 'img/docusaurus.png',
-
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
-
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
-
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
 };
 
 module.exports = siteConfig;
